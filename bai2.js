bb9a47fc64d45902e27d2b6e7ff3378867033956
quangbao16t1 <bao16t1@gmail.com>
2022-03-18 11:02:20 +0700

    bai2.js

--- bai2.js
+++ bai2.js
@@ -30,5 +30,34 @@ function getSum() {
         console.log(sum);   
     }
 }
-getSum();
+// getSum();
+const listFolder = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function getSumByMap() {
+    listFolder.map(i => {
+        let startDate = new Date().getTime();
+        let endDate = startDate;
+        
+        while (endDate - startDate < 1000) {
+            endDate = new Date().getTime();
+        }
+
+        let pathFile1 = path.join('../', 'folder', String(i), `file1.txt`);
+        const data1 = fs.readFileSync(pathFile1, "utf8",  (err) => {
+            if(err) {
+                throw err;
+            }
             
+        });
+        let pathFile2 = path.join('../', 'folder', String(i), `file2.txt`);
+        const data2 = fs.readFileSync(pathFile2, "utf8",  (err) => {
+            if(err) {
+                throw err;
+            }
+        });  
+        sum += Number(data1) + Number(data2);     
+        console.log(sum); 
+    });
+    console.log("Sum: "+sum);
+          
+}
+getSumByMap();
